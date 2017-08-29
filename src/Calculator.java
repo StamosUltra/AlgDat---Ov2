265a9109f0645032be063d4e193d104a9743b3eb
StamosUltra <joaki.m@hotmail.com>
2017-08-29 13:12:45 +0200

    Last commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,19 +4,19 @@
  * This class contains one static method that works recursively
  */
 public class Calculator {
-    public static double exponentAlgorithm(int x, int n) {
+    public static double exponentAlgorithm(double x, int n) {
         if (n == 0) return 1L;
         else return x * exponentAlgorithm(x, n-1);
     }
 
-    public static double exponentAlgorithm2(int x, int n) {
+    public static double exponentAlgorithm2(double x, int n) {
         if (n == 0) return 1;
         else if (n % 2 != 0) { // checks to see if it's an odd number
-            return x * exponentAlgorithm2(x, (n-1)/2) * exponentAlgorithm2(x, (n-1)/2);
+            return x * exponentAlgorithm2(x*x, (n-1)/2);
 
         }
         else { // else it's a even number
-            return exponentAlgorithm2(x, n/2) * exponentAlgorithm2(x, n/2);
+            return exponentAlgorithm2(x*x, n/2);
         }
     }
 }
